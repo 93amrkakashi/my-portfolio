c7edff468f76fda0b6acabe2c15c34fc2dbfdd16
Amr Ali <110984363+93amrkakashi@users.noreply.github.com>
2022-09-24 02:40:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ switchs.addEventListener("click",switching);
 
 
 window.onscroll = function () {
-  if (window.scrollY >= section.offsetTop-120) {
+  if (window.scrollY >= section.offsetTop-140) {
     console.log("Reached Section Three");
     spans.forEach((span) => {
       span.style.width = span.dataset.width;
@@ -44,6 +44,25 @@ window.onscroll = function () {
     })
   }
 };
+
+
+
+
+
+function toggleTheme() { 
+  // Obtains an array of all <link> 
+  // elements. 
+  // Select your element using indexing. 
+  let theme = document.getElementsByTagName('link')[1]; 
+
+  // Change the value of href attribute  
+  // to change the css sheet. 
+  if (theme.getAttribute('href') == 'dark.css') { 
+      theme.setAttribute('href', 'light.css'); 
+  } else { 
+      theme.setAttribute('href', 'dark.css'); 
+  } 
+} 
 
 
 
